1107ce55c35de90e2768e0d3f2619a756f2a5ac5
doroh228 <doroh.2001@gmail.com>
2021-11-02 13:20:16 +0300

    add new method - getsAllLinksOnProducts()

--- main.rb
+++ main.rb
@@ -4,25 +4,37 @@ require 'csv'
 require 'open-uri'
 #require_relative ''
 
-select_url ='https://www.petsonic.com/farmacia-para-gatos/'
+Select_url ='https://www.petsonic.com/farmacia-para-gatos/'
 
 def load_url(url)
+  return Nokogiri::HTML(Curl.get(url).body_str)
+end
+def loadUrlWithPage(url) # Get HTML code
   return Nokogiri::HTML(URI.open(url))
 end
-
 def countOfPages (url) # How many pages
   countOfProducts = load_url(url).xpath("//input[@id = 'nb_item_bottom']/@value").text.to_i
   return (countOfProducts/25.00).ceil
 end
 
 def urlPage(url,page_n) # open html with select page
-  load_url("#{url}?p=#{page_n.to_s}")
+  loadUrlWithPage("#{url}?p=#{page_n.to_s}")
 end
 
 def getLinksFromPage(url_page) # gets links from select url
+  links = []
   url_page.xpath("//div[@class='product-desc display_sd']//a//@href").each do|link|
-    puts link.content
+    links << link.content
   end
+  return links
 end
-p countOfPages(select_url)
-# getLinksFromPage(urlPage(select_url,1))
+def getsAllLinksOnProduts() # Get all links on products
+  links =[]
+  for i in (1..countOfPages(Select_url))
+    url_selectPage = urlPage(Select_url,i)
+    links+=getLinksFromPage(url_selectPage)
+  end
+  return links
+end
+p countOfPages(Select_url)
+p getsAllLinksOnProduts.length
