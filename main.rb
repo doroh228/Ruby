3b9b1ffe2dc61a4533cb6fdb9e60dc64ad1f4915
doroh228 <doroh.2001@gmail.com>
2021-11-02 21:13:54 +0300

    Task done. But should be edit on OOP concept.

--- main.rb
+++ main.rb
@@ -2,11 +2,13 @@ require 'curb'
 require 'nokogiri'
 require 'csv'
 require 'open-uri'
+require 'ruby-progressbar'
 #require_relative ''
 
-Select_url ='https://www.petsonic.com/farmacia-para-gatos/'
+$select_url =""
 
 def load_url(url)
+  $select_url = url
   return Nokogiri::HTML(Curl.get(url).body_str)
 end
 def loadUrlWithPage(url) # Get HTML code
@@ -30,11 +32,57 @@ def getLinksFromPage(url_page) # gets links from select url
 end
 def getsAllLinksOnProduts() # Get all links on products
   links =[]
-  for i in (1..countOfPages(Select_url))
-    url_selectPage = urlPage(Select_url,i)
+  for i in (1..countOfPages($select_url))
+    url_selectPage = urlPage($select_url,i)
     links+=getLinksFromPage(url_selectPage)
   end
   return links
 end
-p countOfPages(Select_url)
-p getsAllLinksOnProduts.length
+
+def getDataAboutProduct(url)
+  htmlSelectPtoduct = loadUrlWithPage(url)
+  title = htmlSelectPtoduct.xpath("//div[@class='nombre_fabricante_bloque col-md-12 desktop']//h1").text.to_s.strip
+  image = htmlSelectPtoduct.xpath("//div[@id='image-block']//img//@src").map { |p| p.text }
+  criteria = htmlSelectPtoduct.xpath("//ul[@class='attribute_radio_list pundaline-variations']//li//span[@class='radio_label']").map { |p| p.text }
+  prices = htmlSelectPtoduct.xpath("//ul[@class='attribute_radio_list pundaline-variations']//li//span[@class='price_comb']").map { |p| p.text.to_s.delete("€").strip }
+  products = []
+  (0...criteria.length).each do |i|
+    products.push({
+                    title: criteria[i].nil? ? title : title + ' - ' + criteria[i],
+                    price: prices[i],
+                    image: image[0],
+                  })
+  end
+  return products
+end
+
+if ARGV.length != 2
+  puts "Wrong number of arguments(given #{ARGV.length}, expected 2) (ArgumentError)"
+  exit
+end
+
+urlCategory = ARGV[0]
+csv_file = ARGV[1]
+
+load_url(urlCategory)
+links = getsAllLinksOnProduts()
+
+total_items = 0
+
+progressbar = ProgressBar.create(title: "Grabbed", format: "%t %c/%C products: |%b>%i| %E", total: links.length)
+column_header = ["Name","Price","Image"]
+CSV.open(csv_file, "w", :write_headers=> true, :headers => column_header) {}
+
+
+links.each do |link|
+  result = getDataAboutProduct(link)
+  CSV.open(csv_file, "a",) do |csv|
+    result.each do |p|
+      csv << [p[:title], p[:price], p[:image]]
+    end
+  end
+  total_items += result.length
+  progressbar.increment
+end
+
+puts "Grabbed #{total_items} items from #{links.length} product pages"
