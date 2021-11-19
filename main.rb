34571ea5dcb03f2b23f736f04944e927ef34bb08
sergey <doroh.2001@gmai.com>
2021-11-19 15:22:26 +0300

    added YAML and finished with this task.

--- main.rb
+++ main.rb
@@ -4,37 +4,12 @@ require 'csv'
 require 'ruby-progressbar'
 require_relative 'Modules/parser_container'
 require_relative 'Models/parser'
+require 'yaml'
 
 
-if ARGV.length != 2
-  puts "Wrong number of arguments(given #{ARGV.length}, expected 2) (ArgumentError)"
-  exit
-end
+params = YAML.load_file('params.yml')
 
-parser = Parser.new(ARGV[0])
-parser.ResultOutput(ARGV[0],ARGV[1]);
+parser = Parser.new(params['category'])
+parser.ResultOutput(params['category'],params['file'])
 
-=begin
-class Apple
-  attr_accessor :name, :weight, :color
-
-  def initialize (name, weight, color)
-    @name, @weight, @color = name, weight, color
-  end
-end
-CSV.open(ARGV[1], "w", :write_headers=> true, :headers => ["Name","Weight","Color"]) {}
-apples = []
-
-(0..10).each do |i|
-  someApple = Apple.new("name"+i.to_s, (i*rand(10)).to_s + " g", "color"+i.to_s)
-  apples << someApple
-end
-
-
-CSV.open(ARGV[1], "a") do |csv|
-  apples.each { |apple|
-    csv << [apple.name, apple.weight, apple.color]
-  }
-end
-=end
 
